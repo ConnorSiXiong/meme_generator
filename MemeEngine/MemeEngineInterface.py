f1d3b13e50e9475501e965a233b14d856b893789
AlexSiXiong <2428545784@qq.com>
2022-01-20 12:01:36 +1300

    adjust functions in the interface

--- MemeEngine/MemeEngineInterface.py
+++ MemeEngine/MemeEngineInterface.py
@@ -1,11 +1,16 @@
 from abc import ABC, abstractmethod
-from utils import dir_existence_checker
+from helper import dir_existence_checker
 
 
 class MemeEngineInterface(ABC):
     def __init__(self, output_dir):
         dir_existence_checker(output_dir)
         self.output_dir = output_dir
+        self.support_format = ['jpg', 'jpeg', 'png']
+
+    def can_ingest(self, path: str):
+        if not path.split('.')[-1] in self.support_format:
+            raise Exception("cannot ingest exception")
 
     @abstractmethod
     def make_meme(self,img_path: str, text: str, author: str, width=500) -> str:
